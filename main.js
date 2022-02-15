6daddba2b3a97f2c017d4249d62f87ac53a85650
Jana Haganová <jhaganova@gmail.com>
2022-02-15 19:00:58 +0100

    add 2x mouseover event on squares

--- main.js
+++ main.js
@@ -5,3 +5,12 @@ nadpis.classList.add('zluty');
 
 let ctverecek = document.querySelector('div.zluty');
 ctverecek.classList.add('ctverecek');
+
+let pocitadlo = 0;
+
+function priNajetiNaCtverec() {
+    console.log('test' + pocitadlo);
+    //pocitadlo = pocitadlo +1;
+    pocitadlo++;
+}
+
